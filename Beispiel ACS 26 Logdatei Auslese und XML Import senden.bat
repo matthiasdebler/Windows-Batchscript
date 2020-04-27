f23ec735300060aa248b432ce03761761d2e2ce5
Matthias Debler <31104972+matthiasdebler@users.noreply.github.com>
2020-04-27 09:05:32 +0200

    Create Beispiel ACS 26 Logdatei Auslese und XML Import senden.bat
    Beispiel ACS 26 Logdatei (Auslese und XML Import) senden

---
+++ Beispiel ACS 26 Logdatei Auslese und XML Import senden.bat
@@ -0,0 +1,16 @@
+rem 
+rem path Variable eintragen in Systemsteuerung!
+rem erzeuge kopie zum versand nach c:\ mittels blat.exe
+rem
+xcopy "C:\Programme\Siemeca\Acs26 2.2\acs26_scheduler.log"   "C:\" /Y
+rem
+Blat -install backup.host  exad@mailhost.de
+rem
+rem
+blat "C:\acs26_scheduler.log" -to exad@mailhost.de  -server domino -subject "ACS 26 Logdatei taegliche Auslese  Gateways"
+rem
+del C:\acs26_scheduler.log
+del "C:\Programme\Siemeca\Acs26 2.2\acs26_scheduler.log"
+rem loesche alte Logdatei und kopiere neue Vorlage ins Verzeichnis 
+xcopy "c:\Programme\Siemeca\scheduler_datei_leer\acs26_scheduler.log" "C:\Programme\Siemeca\Acs26 2.2\"
+rem
