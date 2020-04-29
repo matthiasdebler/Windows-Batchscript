b9575cea11d361bba565a4f3693e87610e6b657f
Matthias Debler <31104972+matthiasdebler@users.noreply.github.com>
2020-04-29 13:05:41 +0200

    Add files via upload

---
+++ Recursives_löschen.cmd
@@ -0,0 +1,13 @@
+
+
+rem	**********************************
+rem	****                          ****
+rem	**** Löschen alter Logdateien ****
+rem	****                          ****
+rem	**********************************
+rem
+C:\delage32\delage32.exe "C:\ILEC\log\Export\*.*" 7 /recurse
+
+
+
+
